7142c2f51fc4d1603daa7a37f9b497a94a963e9d
BenjaUrquia <urq23001@byui.edu>
2025-03-20 19:50:16 -0300

    Fixed errors

--- scripts/index.js
+++ scripts/index.js
@@ -48,7 +48,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-
     // Visitor Count
     const visitCounter = document.getElementById("visitCounter");
     let visits = localStorage.getItem("pageVisits");
